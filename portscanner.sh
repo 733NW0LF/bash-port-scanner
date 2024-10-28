717f254227cee34c426c2329e351a2c6d18a43e2
Aswin  Krishna <78546318+733NW0LF@users.noreply.github.com>
2024-10-28 16:37:19 +0530

    port scanner file

---
+++ portscanner.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <IP-ADDRESS>"
+  exit 1
+fi
+
+ip="$1"
+
+start_port=1
+end_port=100
+
+echo "Scanning ports on $ip..."
+
+
+for port in $(seq $start_port $end_port); do
+
+  nc -z -w1 $ip $port 2>/dev/null
+  if [ $? -eq 0 ]; then
+    echo "Port $port is open"
+  fi
+done
+
+echo "Scan completed on $ip."
